6d60d826a21d55cd8dde1ba5f0f241ec0f3c0a7f
Yuvraj-H3R3 <cyuvraj980@gmail.com>
2025-01-19 12:26:10 +0530

    completed project

--- rock-paper-scissor/script.js
+++ rock-paper-scissor/script.js
@@ -1,12 +1,12 @@
 const userDisplay = document.getElementById('user-ans');
 const cpuDisplay = document.getElementById('cpu-ans');
-const userScore = document.getElementById('user');
-const cpuScore = document.getElementById('cpu');
-const user = 0;
-const cpu = 0;
+const userScore = document.getElementById('user-score');
+const cpuScore = document.getElementById('cpu-score');
+let user = 0;
+let cpu = 0;
 
-tempUser;
-tempCpu;
+let tempUser;
+let tempCpu;
 
 function change(){
     let rando = Math.floor(Math.random()*3)+1;
@@ -24,19 +24,50 @@ function change(){
     }
 }
 
-function check(e){
+function updateScore(){
+    userScore.innerHTML = user;
+    cpuScore.innerHTML = cpu;
+}
 
-    if(e==1){
-        userDisplay.innerHTML = "Rock";
-        cpuDisplay.innerHTML = change();
+function changeScore(tempCpu , tempUser){
+    if((tempUser==="Paper" && tempCpu==="Rock" ) || (tempUser==="Rock" && tempCpu==="Scissor") || (tempUser==="Scissor" && tempCpu==="Rock")){
+        user += 1; 
     }
-    else if(e==2){
-        userDisplay.innerHTML = "Paper";
-        cpuDisplay.innerHTML = change();
-    }
-    else{
-        userDisplay.innerHTML = "Scissor";
-        cpuDisplay.innerHTML = change();
+    else if((tempUser==="Paper" && tempCpu==="Paper" ) || (tempUser==="Scissor" && tempCpu==="Scissor") || (tempUser==="Rock" && tempCpu==="Rock")){
+        
+    }else{
+        cpu += 1;
     }
 }
 
+function check(e){
+
+    if(e==1){
+        tempUser = "Rock";
+        userDisplay.innerHTML = "Rock";
+        cpuDisplay.innerHTML = change();
+        changeScore(tempCpu , tempUser);
+        updateScore()
+    }
+    else if(e==2){
+        tempUser = "Paper";
+        userDisplay.innerHTML = "Paper";
+        cpuDisplay.innerHTML = change();
+        changeScore(tempCpu , tempUser);
+        updateScore()
+    }
+    else{
+        tempUser = "Scissor";
+        userDisplay.innerHTML = "Scissor";
+        cpuDisplay.innerHTML = change();
+        changeScore(tempCpu , tempUser);
+        updateScore()
+    }
+}
+
+function resetScore(){
+    user = 0;
+    cpu = 0;
+    userScore.innerHTML = 0;
+    cpuScore.innerHTML = 0;
+}
