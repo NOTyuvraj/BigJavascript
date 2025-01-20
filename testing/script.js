0d2fd319c758e624cd5b51f5e1a46d9178fed7ce
Yuvraj-H3R3 <cyuvraj980@gmail.com>
2025-01-20 23:36:35 +0530

    damage control

---
+++ testing/script.js
@@ -0,0 +1,10 @@
+const container = document.getElementById('container');
+const content = document.querySelector('.content');
+
+content.innerHTML = `<p style ="color : red ;">Hey I'm Red</p>
+    <h3 style = "color:blue;">Im blue h3</h3>
+    <div>
+        <h1>Im in a div</h1>
+        <p>Me Too!</p>
+    </div>
+`;
