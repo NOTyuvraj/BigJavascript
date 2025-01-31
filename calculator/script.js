19f07a0088e84bc0aa169648f3dac2b4d7621c5a
Yuvraj-H3R3 <cyuvraj980@gmail.com>
2025-02-01 00:34:57 +0530

    completed calculator
    added every other functionality

--- calculator/script.js
+++ calculator/script.js
@@ -1,14 +1,25 @@
-
 const display = document.querySelector("#display");
 const btnInput = document.querySelectorAll(".btns");
 
-btnInput.forEach(function(ele){
-    ele.addEventListener('click' , function(){
-        display.value += ele.innerHTML
-    })
-})
+btnInput.forEach(function (ele) {
+  ele.addEventListener("click", function () {
+    display.value += ele.innerHTML;
+  });
+});
 
-function calculate(){
+function calculate() {
+  try {
     const result = math.evaluate(display.value);
     display.value = result;
+  } catch (err) {
+    display.value = "Error";
+  }
+}
+
+function clearAll(){
+    display.value = "";
+}
+
+function clearOne(){
+    display.value = display.value.slice(0,-1);
 }
