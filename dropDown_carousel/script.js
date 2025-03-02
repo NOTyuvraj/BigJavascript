9272828110972a6c28f94c655321c65fd634efa2
Yuvraj-H3R3 <cyuvraj980@gmail.com>
2025-03-02 17:15:06 +0530

    added extra functionality
    carousel is finally completed

--- dropDown_carousel/script.js
+++ dropDown_carousel/script.js
@@ -1,32 +1,59 @@
 
-
-
-const images = document.querySelectorAll(".carouselImages");
+const menuBtn = document.getElementById("menuBtn");
+const dropDown = document.querySelector(".menuDropDown");
 const prevBtn = document.querySelector(".prev");
-const nextBtn = document.querySelector(".next")
-const menuBtn = document.querySelector("#menuBtn");
-const menuDrop = document.querySelector(".menuDropDown");
+const nextBtn = document.querySelector(".next");
+const carouselImages = document.querySelectorAll(".carouselImages");
+const placeHolder = document.querySelectorAll('.placeHolder');
+
 let currentIndex = 0;
 
-menuBtn.addEventListener("click" , function(){
-    menuDrop.classList.toggle("menuDisplay")
-})
+menuBtn.addEventListener("click" , function() {
+    dropDown.classList.toggle('menuDisplay');
+});
 
-function updateCarousel(){
-    images.forEach(function(img , index){
-        img.style.display = index === currentIndex ? "block" : "none";
-    });
+function updateCarousel(index){
+    carouselImages.forEach(function(image , i){
+        image.classList.remove('shown');
+        if(i === index){
+            image.classList.add('shown');
+        }
+        currentIndex = index;
+    })
 }
 
-prevBtn.addEventListener("click" , function(){
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    updateCarousel();
+function updatePlaceHolder(index){
+    placeHolder.forEach(function(place , i){
+        place.classList.remove('currentSlidePlaceHolder');
+        if(i === index ){
+            place.classList.add('currentSlidePlaceHolder');
+        }
+        currentIndex = index;
+    })
+}
+
+function nextSlide(){
+    const nextIndex = (currentIndex + 1) % carouselImages.length;
+    updateCarousel(nextIndex);
+    updatePlaceHolder(nextIndex);
+}
+
+function prevSlide(){
+    const prevIndex = (currentIndex - 1 + carouselImages.length) % carouselImages.length;
+    updateCarousel(prevIndex);
+    updatePlaceHolder(prevIndex);
+}
+
+placeHolder.forEach((ele , i)=>{
+    ele.addEventListener('click' , function(){
+        let currIndex = ele.getAttribute('index');
+        updateCarousel(Number(currIndex));
+        updatePlaceHolder(Number(currIndex));
+    })
 })
 
+updateCarousel(currentIndex);
+setInterval( nextSlide , 5000);
 
-nextBtn.addEventListener("click" , function(){
-    currentIndex = (currentIndex + 1 + images.length) % images.length;
-    updateCarousel();
-})
-
-updateCarousel();
+nextBtn.addEventListener("click" , nextSlide);
+prevBtn.addEventListener("click" , prevSlide);
